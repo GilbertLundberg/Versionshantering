b6c6bb9de4ada2ed43b7fcd38c88005109c8f830
Gilbert Lundberg Rydbäck <gilbert.lundberg.rydback@iths.se>
2022-09-25 10:33:02 +0200

    Added code.

--- Versionshantering/Program.cs
+++ Versionshantering/Program.cs
@@ -1,2 +1,71 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string[] techniques = { "   C#", "daTAbaser", "WebbuTVeCkling ", "clean Code   " };
+string[] messagesToClass = { "Glöm inte att övning ger färdighet!", "Öppna boken på sida 257." };
+string className = "Klass 2022";
+
+WebsiteGenerator website = new WebsiteGenerator(className, messagesToClass, techniques);
+
+website.PrintPage();
+
+class WebsiteGenerator
+{
+    private string[] techniques;
+    private string[] messageToClass;
+    private string className;
+
+    public WebsiteGenerator(string className, string[] messageToClass, string[] techniques)
+    {
+        this.className = className;
+        this.messageToClass = messageToClass;
+        this.techniques = techniques;
+    }
+
+    public void PrintPage()
+    {
+        PrintStart();
+        PrintHeader();
+        PrintCourses();
+        PrintEnd();
+    }
+
+
+    void PrintStart()
+    {
+        string start = "<!DOCTYPE html>\n<html>\n<body>\n<main>\n";
+        Console.WriteLine(start);
+    }
+
+    private void PrintHeader()
+    {
+        Console.WriteLine($"<h1>Välkomna {this.className}</h1>");
+        foreach (string msg in this.messageToClass)
+        {
+            Console.WriteLine($"<p><b>Meddelande: </b>{msg}</p>");
+        }
+    }
+
+    void PrintCourses()
+    {
+        string kurser = CourseGenerator(this.techniques);
+        Console.WriteLine(kurser);
+    }
+
+
+    private void PrintEnd()
+    {
+        string end = "</main>\n</body>\n</html>";
+        Console.WriteLine(end);
+    }
+
+    string CourseGenerator(string[] techniques)
+    {
+        string kurser = "";
+
+        foreach (string technique in techniques)
+        {
+            string tmp = technique.Trim();
+            kurser += "<p>" + tmp[0].ToString().ToUpper() + tmp.Substring(1).ToLower() + "</p>\n";
+        }
+
+        return kurser;
+    }
+}
